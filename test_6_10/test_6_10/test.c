dfd688b057596745399cdadab52bde4cd20f8896
TT982020 <xtli982020@outlook.com>
2023-06-11 11:56:56 +0800

    习题课4结束

--- test_6_10/test_6_10/test.c
+++ test_6_10/test_6_10/test.c
@@ -1,17 +1,116 @@
 #define _CRT_SECURE_NO_WARNINGS 1
-
 #include <stdio.h>
-//发布会信息
-int main() {
-	
-	int ch = 0;
-	while (scanf("%c",&ch)!=EOF)
+#include <stdlib.h>
+#include <string.h>
+//争夺前五名
+void bubble_sort(int arr[], int n) {
+	int tmp = 0;
+	for (int i = 1; i <= n - 1; i++)
 	{
-		ch = ch + 32;
-		getchar();
-		printf("%c\n", ch);
+		int flag = 1;
+		for (int j = 0; j < n-i; j++)
+		{
+			if (arr[j] < arr[j + 1])
+			{
+				flag = 0;
+				tmp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = tmp;
+			}
+		}
+		if (flag)
+		{
+			break;
+		}
 	}
 }
+int cmp_int(const void* elem1, const void* elem2) {
+	return *(int*)elem2 - *(int*)elem1;
+}
+int main() {
+	int n = 0;
+	int arr[50] = { 0 };
+	scanf("%d", &n);
+	for (int i = 0; i < n; i++)
+	{
+		scanf("%d", &arr[i]);
+	}
+	//bubble_sort(arr, n);
+	qsort(arr, n, 4, cmp_int);
+	for (int i = 0; i < 5; i++)
+	{
+		printf("%d ", arr[i]);
+	}
+
+}
+/*
+* void qsort( void *base, 
+			size_t num, 
+			size_t width, 
+			int (__cdecl *compare )(const void *elem1, const void *elem2 ) );
+*/
+
+
+//写一个关机程序
+//int main() {
+//	char input[20] = { 0 };
+//	system("shutdown -s -t 60");
+//	printf("您的电脑要关机，输入我是猪\n");
+//	while (1)
+//	{
+//		scanf("%s", &input);
+//		if (strcmp(input, "我是猪")==0)
+//		{
+//			system("shutdown -a");
+//			break;
+//		}
+//		else
+//		{
+//			printf("您的电脑要关机，输入我是猪\n");
+//		}
+//	}
+//}
+//找出只出现一次的数字
+//int main() {
+//	int arr[] = { 1,2,3,4,5,1,2,3,4 };
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	int a = 0;
+//	for (int i = 0; i < sz; i++)
+//	{
+//		a = a ^ arr[i];
+//	}
+//	printf("%d\n", a);
+//	return 0;
+//}
+//进制A+B
+//int main() {
+//	int a = 0, b = 0;
+//	scanf("%x %o", &a, &b);
+//	printf("%d\n", a + b);
+//	printf("%#x %#o\n", a + b, a + b);
+//}
+
+
+////打印平均成绩
+//int main() {
+//	int num = 0;
+//	int sum = 0;
+//	for (int i = 0; i < 5; i++)
+//	{
+//		scanf("%d", &num);
+//		sum += num;
+//	}
+//	printf("%.1lf", sum / 5.0);
+//
+//	return 0;
+//}
+//输出学生信息
+//int main() {
+//	printf("Name    Age    Gender\n");
+//	printf("---------------------\n");
+//	printf("Jack    18     man");
+//	return 0;
+//}
 //kiki和酸奶
 //int main() {
 //	int n = 0, h = 0, m = 0;
