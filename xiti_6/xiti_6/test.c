a4b57c8274576038a757612226a00fbfe8b85485
TT982020 <xtli982020@outlook.com>
2023-06-13 00:47:18 +0800

    习题6结束

--- xiti_6/xiti_6/test.c
+++ xiti_6/xiti_6/test.c
@@ -1,10 +1,89 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include <stdio.h>
 #include <math.h>
-//获得月份的天数
+//直角三角形图案
 int main() {
-
+    int n = 0;
+    while (~scanf("%d", &n)) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i + 1; j++) {
+                printf("%c ", '*');
+            }
+            printf("\n");
+        }
+    }
 }
+//正方形图案
+//int main() {
+//    int n = 0;
+//    while (~scanf("%d", &n)) {
+//        for (int i = 0; i < n; i++) {
+//            for (int j = 0; j < n; j++) {
+//                printf("%c ", '*');
+//            }
+//            printf("\n");
+//        }
+//    }
+//}
+//线段图案
+//int main() {
+//    int n = 0;
+//    while (~scanf("%d", &n)) {
+//        for (int i = 0; i < n; i++) {
+//            printf("%c", '*');
+//        }
+//        printf("\n");
+//    }
+//}
+//简单计算器
+//int main() {
+//	double n1 = 0.0, n2 = 0.0;
+//	char op = 0;
+//	while (~scanf("%lf %c %lf", &n1, &op, &n2)) {
+//		switch (op)
+//		{
+//		case '+':
+//			printf("%.4lf%c%.4lf=%.4lf\n", n1, '+', n2, n1 + n2);
+//			break;
+//		case '-':
+//			printf("%.4lf%c%.4lf=%.4lf\n", n1, '-', n2, n1 - n2);
+//			break;
+//		case '*':
+//			printf("%.4lf%c%.4lf=%.4lf\n", n1, '*', n2, n1 * n2);
+//			break;
+//		case '/':
+//			if (n2==0)
+//			{
+//				printf("Wrong!Division by zero!\n");
+//			}
+//			else {
+//				printf("%.4lf%c%.4lf=%.4lf\n", n1, '/', n2, n1 / n2);
+//			}
+//			break;
+//		default:
+//			printf("Invalid operation!\n");
+//			break;
+//		}
+//	}
+//	return 0;
+//}
+//获得月份的天数
+//int main() {
+//	int year = 0, month = 0;
+//	int days[12] = { 31,28,31,30,31,30,31,31,30,31,30,31 };
+//	while (~scanf("%d %d", &year, &month))
+//	{
+//		int day = days[month - 1];
+//		if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
+//		{
+//			if (month==2)
+//			{
+//				day += 1;
+//			}
+//		}
+//		printf("%d\n", day);
+//	}
+//}
 //计算一元二次方程
 //int main() {
 //	float a = 0.0, b = 0.0, c = 0.0;
