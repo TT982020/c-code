e74940eb9c59f35dc07c981daa492e03db9fd0a3
TT982020 <xtli982020@outlook.com>
2023-07-18 23:18:30 +0800

    预处理

---
+++ test_7_18/test_7_18/test.c
@@ -0,0 +1,43 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include "test.h"
+#define M 100
+#undef M
+#define SQUARE(X) ((X)*(X))
+#define PRINT(N) printf("the value of "#N" is %d.\n",N)
+#define CAT(CLASS,NUM) CLASS##NUM
+#define MALLOC(num,type) (type*)malloc((num)*sizeof(type))
+void test1() {
+	int r = 5;
+	printf("%d\n", SQUARE(5+1));
+}
+
+void test2() {
+	//printf("hello ""world.");
+	int a = 10, b = 20;
+	PRINT(a);
+	PRINT(b);
+}
+
+void test3() {
+	int class09 = 100;
+	printf("%d\n", CAT(class, 09));
+	int sum5 = 0;
+	for (int i = 0; i < 10; i++)
+	{
+		CAT(sum, 5) += i;
+	}
+	printf("%d\n", sum5);
+}
+void test4() {
+	int* p = MALLOC(10, int);
+	free(p);
+	p = NULL;
+	
+}
+int main() {
+	//test1();
+	//test2();
+	//test3();
+	test4();
+}
---
+++ test_7_18/test_7_18/test.h
@@ -0,0 +1,5 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#ifndef __TEST_H__
+#define __TEST_H__
+int add(int a, int b);
+#endif // !__TEST_H__
